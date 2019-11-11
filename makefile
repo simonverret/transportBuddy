15c7a600037fe92be3f3cfa1ed114b383956834e
Maxime Charlebois <mnmpdadish@hotmail.com>
2019-11-11 18:47:33 -0500

    reversing loop, slower?

--- makefile
+++ makefile
@@ -5,6 +5,6 @@ COMPILER = gcc
 # COMPILER = $(HOME)/bin/faster_clang
 
 all: src/transportBuddy.c
-	$(COMPILER) $(OPTIONS) -o $(EXEC) src/transportBuddy.c
+	$(COMPILER) $(OPTIONS) -o $(EXEC) src/transportBuddy.c -lm
 install : all
 	cp $(EXEC) $(INSTALLDIR)
--- src/transportBuddy.c
+++ src/transportBuddy.c
@@ -194,6 +194,7 @@ int main(int argc, const char * argv[]) {
         double kz = M_PI*(kk*2.0/(nKz)); // 0 to 2Pi assumes parity (periodicity in z is 4Pi because of the tetragonal structure)
         coskz[kk] = cos(kz); coskz_2[kk] = cos(kz/2.); sinkz_2[kk] = sin(kz/2.);
     }
+
     
     //// RESULTS TO FILL
     double dos[nMu];
@@ -213,11 +214,13 @@ int main(int argc, const char * argv[]) {
     double beta_zz[nMu][nT];
     double beta_xy[nMu][nT];
     
+    int iMu;
+    
     //// LOOP ON mu (chemical potential)
-    for(int iMu=0; iMu<nMu; iMu++)
+    double mu[nMu];
+    for(iMu=0; iMu<nMu; iMu++)
     {
-        double mu = muMin;
-        if (muMin!=muMax) mu += iMu*(muMax-muMin)/(nMu-1);
+        if (muMin!=muMax) mu[iMu] = muMin + iMu*(muMax-muMin)/(nMu-1);
         
         //// INITIALIZE RESULTS TO 0
         dos[iMu] = 0.;
@@ -240,129 +243,136 @@ int main(int argc, const char * argv[]) {
             beta_xx[iMu][nn]=0.;
             beta_zz[iMu][nn]=0.;
             beta_xy[iMu][nn]=0.;
-        }
-        
-        //// INTEGRAL ON k
-        for(int ii=0; ii<nK; ii++) {
-            printf("muu= %i / %i, k = %i / %i \n",iMu,nMu,ii,nK);//fflush(stdout);
-            for(int jj=0; jj<nK; jj++) {
-                
-                double epsilon_k   = -mu;
-                epsilon_k         += -2.*t    * (cosk[ii] + cosk[jj]);
-                epsilon_k         += -4.*tp   *  cosk[ii]*cosk[jj];
-                epsilon_k         += -2.*tpp  * (cos2k[ii] + cos2k[jj]);
-                epsilon_k         += -4.*tppp * (cos2k[ii]*cosk[jj] + cosk[ii]*cos2k[jj]);
-                double depsilon_dkx = 0;
-                depsilon_dkx      +=  2.*t    *  sink[ii];
-                depsilon_dkx      +=  4.*tp   *  sink[ii]*cosk[jj];
-                depsilon_dkx      +=  4.*tpp  *  sin2k[ii];
-                depsilon_dkx      +=     tppp * (8.*sin2k[ii]*cosk[jj] + 4.*sink[ii]*cos2k[jj]);
-                double depsilon_dkx_dky = 0;
-                depsilon_dkx_dky  += -4.*tp   *  sink[ii]*sink[jj];
-                depsilon_dkx_dky  += -8.*tppp * (sin2k[ii]*sink[jj] + sink[ii]*sin2k[jj]);
-                double depsilon_dky = 0;
-                depsilon_dky      +=  2.*t    *  sink[jj];
-                depsilon_dky      +=  4.*tp   *  sink[jj]*cosk[ii];
-                depsilon_dky      +=  4.*tpp  *  sin2k[jj];
-                depsilon_dky      +=     tppp * (8.*sin2k[jj]*cosk[ii] + 4.*sink[jj]*cos2k[ii]);
-                double depsilon_dky_dky = 0;
-                depsilon_dky_dky  +=  2.*t    *  cosk[jj];
-                depsilon_dky_dky  +=  4.*tp   *  cosk[jj]*cosk[ii];
-                depsilon_dky_dky  +=  8.*tpp  *  cos2k[jj];
-                depsilon_dky_dky  +=     tppp * (16.*cos2k[jj]*cosk[ii] + 8.*cosk[jj]*cos2k[ii]);
-                
-                // This will not work outside the first quadrant of the Brillouin zone
-                double cos_phi12 = 0.0;
-                if (ii > 0 & jj > 0 & (ETAk > 0 || ETAkw > 0)) {
-                    double cos_phi = cos(atan(2*jj/ii));
-                    cos_phi12 = pow(cos_phi,12);
-                }
+        }    
+    }
 
-                for(int kk=0; kk<nKz; kk++) {
-                    
-                    // See Markiewicz 2005 for the 3D tight-binding model // derivatives from mathematica
-                    double epsilonz_k         = -2*tz*coskz_2[kk]*(cosk[ii]-cosk[jj])*(cosk[ii]-cosk[jj])*cosk_2[ii]*cosk_2[jj];
-                    double depsilonz_dkx      = tz*cosk_2[jj]*(-4-5*cosk[ii]+cosk[jj])*(-cosk[ii]+cosk[jj])*coskz_2[kk]*sink_2[ii];
-                    double depsilonz_dky      = tz*cosk_2[ii]*(-4+cosk[ii]-5*cosk[jj])*(cosk[ii]-cosk[jj])*coskz_2[kk]*sink_2[jj];
-                    double depsilonz_dkx_dky  = tz*(1/4.)*(6-5*cos2k[ii]+16*cosk[jj]+4*cosk[ii]*(4+9*cosk[jj])-5*cos2k[jj])*coskz_2[kk]*sink_2[ii]*sink_2[jj];
-                    double depsilonz_dky_dky  = tz*(1/4.)*cosk_2[ii]*cosk_2[jj]*(10+16*cosk[ii]+cos2k[ii]-4*(4+9*cosk[ii])*cosk[jj]+25*cos2k[jj])*coskz_2[kk];
-                    double depsilonz_dkz      = tz*cosk_2[ii]*cosk_2[jj]*(cosk[ii]-cosk[jj])*(cosk[ii]-cosk[jj])*sinkz_2[kk];
-                    
-                    double ep_k        =  epsilon_k   + epsilonz_k;
-                    double dep_dkx     = depsilon_dkx + depsilonz_dkx;
-                    double dep_dky     = depsilon_dky + depsilonz_dky;
-                    double dep_dkx_dky = depsilon_dkx_dky + depsilonz_dkx_dky;
-                    double dep_dky_dky = depsilon_dky_dky + depsilonz_dky_dky;
-                    double dep_dkz     = depsilonz_dkz;
-                    
-                    double Gamma = ETA;
-                    double normV_k = sqrt(dep_dkx*dep_dkx + dep_dky*dep_dky + dep_dkz*dep_dkz);
-                    Gamma += ETAell*normV_k;
-                    Gamma += ETAdos/(normV_k+10e-10);
-                    Gamma += ETAk*cos_phi12;
-                    double Ak_0   = -(1./M_PI)*cimag(1.0/ (I*Gamma - ep_k));
 
-                    //// T-INDEPENDENT
-                    double kernel_xx  = dep_dkx*dep_dkx;
-                    double kernel_zz  = dep_dkz*dep_dkz;
-                    double kernel_xy  = -(2./3.)*(dep_dkx * (dep_dkx * dep_dky_dky - dep_dky * dep_dkx_dky));
-                    
+    
+    //// INTEGRAL ON k
+    for(int ii=0; ii<nK; ii++) {
+        printf("k = %i / %i \n",ii,nK);//fflush(stdout);
+        for(int jj=0; jj<nK; jj++) {
+            
+            double epsilon_k   = 0;//-mu;
+            epsilon_k         += -2.*t    * (cosk[ii] + cosk[jj]);
+            epsilon_k         += -4.*tp   *  cosk[ii]*cosk[jj];
+            epsilon_k         += -2.*tpp  * (cos2k[ii] + cos2k[jj]);
+            epsilon_k         += -4.*tppp * (cos2k[ii]*cosk[jj] + cosk[ii]*cos2k[jj]);
+            double depsilon_dkx = 0;
+            depsilon_dkx      +=  2.*t    *  sink[ii];
+            depsilon_dkx      +=  4.*tp   *  sink[ii]*cosk[jj];
+            depsilon_dkx      +=  4.*tpp  *  sin2k[ii];
+            depsilon_dkx      +=     tppp * (8.*sin2k[ii]*cosk[jj] + 4.*sink[ii]*cos2k[jj]);
+            double depsilon_dkx_dky = 0;
+            depsilon_dkx_dky  += -4.*tp   *  sink[ii]*sink[jj];
+            depsilon_dkx_dky  += -8.*tppp * (sin2k[ii]*sink[jj] + sink[ii]*sin2k[jj]);
+            double depsilon_dky = 0;
+            depsilon_dky      +=  2.*t    *  sink[jj];
+            depsilon_dky      +=  4.*tp   *  sink[jj]*cosk[ii];
+            depsilon_dky      +=  4.*tpp  *  sin2k[jj];
+            depsilon_dky      +=     tppp * (8.*sin2k[jj]*cosk[ii] + 4.*sink[jj]*cos2k[ii]);
+            double depsilon_dky_dky = 0;
+            depsilon_dky_dky  +=  2.*t    *  cosk[jj];
+            depsilon_dky_dky  +=  4.*tp   *  cosk[jj]*cosk[ii];
+            depsilon_dky_dky  +=  8.*tpp  *  cos2k[jj];
+            depsilon_dky_dky  +=     tppp * (16.*cos2k[jj]*cosk[ii] + 8.*cosk[jj]*cos2k[ii]);
+            
+            // This will not work outside the first quadrant of the Brillouin zone
+            double cos_phi12 = 0.0;
+            if (ii > 0 & jj > 0 & (ETAk > 0 || ETAkw > 0)) {
+                double cos_phi = cos(atan(2*jj/ii));
+                cos_phi12 = pow(cos_phi,12);
+            }
+
+            for(int kk=0; kk<nKz; kk++) {
+                
+                // See Markiewicz 2005 for the 3D tight-binding model // derivatives from mathematica
+                double epsilonz_k         = -2*tz*coskz_2[kk]*(cosk[ii]-cosk[jj])*(cosk[ii]-cosk[jj])*cosk_2[ii]*cosk_2[jj];
+                double depsilonz_dkx      = tz*cosk_2[jj]*(-4-5*cosk[ii]+cosk[jj])*(-cosk[ii]+cosk[jj])*coskz_2[kk]*sink_2[ii];
+                double depsilonz_dky      = tz*cosk_2[ii]*(-4+cosk[ii]-5*cosk[jj])*(cosk[ii]-cosk[jj])*coskz_2[kk]*sink_2[jj];
+                double depsilonz_dkx_dky  = tz*(1/4.)*(6-5*cos2k[ii]+16*cosk[jj]+4*cosk[ii]*(4+9*cosk[jj])-5*cos2k[jj])*coskz_2[kk]*sink_2[ii]*sink_2[jj];
+                double depsilonz_dky_dky  = tz*(1/4.)*cosk_2[ii]*cosk_2[jj]*(10+16*cosk[ii]+cos2k[ii]-4*(4+9*cosk[ii])*cosk[jj]+25*cos2k[jj])*coskz_2[kk];
+                double depsilonz_dkz      = tz*cosk_2[ii]*cosk_2[jj]*(cosk[ii]-cosk[jj])*(cosk[ii]-cosk[jj])*sinkz_2[kk];
+                
+                double ep_k        =  epsilon_k   + epsilonz_k;
+                double dep_dkx     = depsilon_dkx + depsilonz_dkx;
+                double dep_dky     = depsilon_dky + depsilonz_dky;
+                double dep_dkx_dky = depsilon_dkx_dky + depsilonz_dkx_dky;
+                double dep_dky_dky = depsilon_dky_dky + depsilonz_dky_dky;
+                double dep_dkz     = depsilonz_dkz;
+                
+                double Gamma = ETA;
+                double normV_k = sqrt(dep_dkx*dep_dkx + dep_dky*dep_dky + dep_dkz*dep_dkz);
+                Gamma += ETAell*normV_k;
+                Gamma += ETAdos/(normV_k+10e-10);
+                Gamma += ETAk*cos_phi12;
+                
+                //// T-INDEPENDENT
+                double kernel_xx  = dep_dkx*dep_dkx;
+                double kernel_zz  = dep_dkz*dep_dkz;
+                double kernel_xy  = -(2./3.)*(dep_dkx * (dep_dkx * dep_dky_dky - dep_dky * dep_dkx_dky));
+                
+                for(int iMu=0; iMu<nMu; iMu++){
+                    double Ak_0 = -(1./M_PI)*cimag(1.0/ (I*Gamma - (ep_k - mu[iMu]) ));
                     dos[iMu]       += Ak_0;
-                    density_0[iMu] += 1.0/(1.0+exp(1000*ep_k));
+                    density_0[iMu] += 1.0/(1.0+exp(1000*(ep_k - mu[iMu]) ));
                     sigmaxx_0[iMu] += kernel_xx * Ak_0*Ak_0;
                     sigmaxy_0[iMu] += kernel_xy * Ak_0*Ak_0*Ak_0;
                     sigmazz_0[iMu] += kernel_zz * Ak_0*Ak_0;
-                    
-                    //// LOOP ON TEMPERATURES
-                    int nn=0; for(nn=0; nn<nT; nn++)
+                }
+
+                //// LOOP ON TEMPERATURES
+                int nn=0; for(nn=0; nn<nT; nn++)
+                {
+                    for(int iMu=0; iMu<nMu; iMu++)  density[iMu][nn]    += 1.0/(1.0+exp(beta[nn] * (ep_k - mu[iMu]) ));
+                    double GammaT = Gamma + ETAaPL*T[nn];
+                           GammaT += M_PI*M_PI*ETAaFL*T[nn]*T[nn];
+                           GammaT += M_PI*M_PI*ETAbFL*T[nn]*T[nn]/(normV_k+10e-10);
+
+                    //// INTEGRAL IN ENERGY
+                    int n=0; for(n=0; n<nOmega; n++)
                     {
-                        density[iMu][nn]    += 1.0/(1.0+exp(beta[nn]*ep_k));
-                        double GammaT = Gamma + ETAaPL*T[nn];
-                               GammaT += M_PI*M_PI*ETAaFL*T[nn]*T[nn];
-                               GammaT += M_PI*M_PI*ETAbFL*T[nn]*T[nn]/(normV_k+10e-10);
-
-                        //// INTEGRAL IN ENERGY
-                        int n=0; for(n=0; n<nOmega; n++)
-                        {
-                            double GammaOmega = GammaT;
-                                   GammaOmega += ETAaFL*omega[nn][n]*omega[nn][n];
-                                   GammaOmega += ETAbFL*omega[nn][n]*omega[nn][n]/(normV_k+10e-10);
-                                   GammaOmega += ETAw*omega[nn][n];
-                                   GammaOmega += ETAkw*omega[nn][n]*cos_phi12;
-                            
-                            double complex z = omega[nn][n] + GammaOmega * I;
-                            double A_k = -(1./M_PI)*cimag(1.0/ (z-ep_k) );
-
-                            double CvKernel = omega[nn][n]*dfermiDirac_dT[nn][n]*A_k;
-                            Cv[iMu][nn]       += CvKernel;
-
-                            double frequencyKernel_xx = -dfermiDirac_dw[nn][n]*kernel_xx*A_k*A_k;
-                            double frequencyKernel_zz = -dfermiDirac_dw[nn][n]*kernel_zz*A_k*A_k;
-                            double frequencyKernel_xy = -dfermiDirac_dw[nn][n]*kernel_xy*A_k*A_k*A_k;
-                            
-                            sigma_xx[iMu][nn] += frequencyKernel_xx;
-                            sigma_zz[iMu][nn] += frequencyKernel_zz;
-                            sigma_xy[iMu][nn] += frequencyKernel_xy;
-                            double betaOmega = beta[nn]*omega[nn][n];
-                            alpha_xx[iMu][nn] += betaOmega * frequencyKernel_xx;
-                            alpha_zz[iMu][nn] += betaOmega * frequencyKernel_zz;
-                            alpha_xy[iMu][nn] += betaOmega * frequencyKernel_xy;
-                            double betaOmega2 = beta[nn]*beta[nn] * omega[nn][n]*omega[nn][n];
-                            beta_xx[iMu][nn]  += betaOmega2 * frequencyKernel_xx;
-                            beta_zz[iMu][nn]  += betaOmega2 * frequencyKernel_zz;
-                            beta_xy[iMu][nn]  += betaOmega2 * frequencyKernel_xy;
+                        double GammaOmega = GammaT;
+                               GammaOmega += ETAaFL*omega[nn][n]*omega[nn][n];
+                               GammaOmega += ETAbFL*omega[nn][n]*omega[nn][n]/(normV_k+10e-10);
+                               GammaOmega += ETAw*omega[nn][n];
+                               GammaOmega += ETAkw*omega[nn][n]*cos_phi12;
+                        
+                        double complex z = omega[nn][n] + GammaOmega * I;
+                        for(int iMu=0; iMu<nMu; iMu++) {
+                          double A_k = -(1./M_PI)*cimag(1.0/ (z-(ep_k - mu[iMu]) ) );
+                          Cv[iMu][nn]       += omega[nn][n]*dfermiDirac_dT[nn][n]*A_k;
+                          
+                          double frequencyKernel_xx = -dfermiDirac_dw[nn][n]*kernel_xx*A_k*A_k;
+                          double frequencyKernel_zz = -dfermiDirac_dw[nn][n]*kernel_zz*A_k*A_k;
+                          double frequencyKernel_xy = -dfermiDirac_dw[nn][n]*kernel_xy*A_k*A_k*A_k;
+                          
+                          sigma_xx[iMu][nn] += frequencyKernel_xx;
+                          sigma_zz[iMu][nn] += frequencyKernel_zz;
+                          sigma_xy[iMu][nn] += frequencyKernel_xy;
+                          double betaOmega = beta[nn]*omega[nn][n];
+                          alpha_xx[iMu][nn] += betaOmega * frequencyKernel_xx;
+                          alpha_zz[iMu][nn] += betaOmega * frequencyKernel_zz;
+                          alpha_xy[iMu][nn] += betaOmega * frequencyKernel_xy;
+                          double betaOmega2 = betaOmega*betaOmega;
+                          beta_xx[iMu][nn]  += betaOmega2 * frequencyKernel_xx;
+                          beta_zz[iMu][nn]  += betaOmega2 * frequencyKernel_zz;
+                          beta_xy[iMu][nn]  += betaOmega2 * frequencyKernel_xy;
                         }
                     }
                 }
             }
         }
+    }
         
+        
+    for(iMu=0; iMu<nMu; iMu++)
+    {
         double f_0 = 2.0/nK/nK/nKz;
-        printf("% 4.8f % 4.8f % 4.8f ", mu, 1.0-f_0*density_0[iMu], f_0*dos[iMu]);
+        printf("% 4.8f % 4.8f % 4.8f ", mu[iMu], 1.0-f_0*density_0[iMu], f_0*dos[iMu]);
         printf("% 4.8f % 4.8f % 4.8f ", f_0*sigmaxx_0[iMu], f_0*sigmaxy_0[iMu], f_0*sigmazz_0[iMu]);
         printf("\n");
         fflush(stdout);
-
         
         //// FILE GROUPED BY T
         fprintf(fileOut, "mu\tn0\tp0\tdos\teta\t");
@@ -377,7 +387,7 @@ int main(int argc, const char * argv[]) {
         nn=0; for(nn=0; nn<nT; nn++)
         {
             double f = (2.*energyCutoff[nn]) /(nOmega)*f_0;
-            fprintf(fileOut,"%e\t%e\t%e\t%e\t%e\t", mu, f_0*density_0[iMu], 1.0-f_0*density_0[iMu], f_0*dos[iMu], etaNorm);
+            fprintf(fileOut,"%e\t%e\t%e\t%e\t%e\t", mu[iMu], f_0*density_0[iMu], 1.0-f_0*density_0[iMu], f_0*dos[iMu], etaNorm);
             fprintf(fileOut,"%e\t%e\t%e\t", f_0*sigmaxx_0[iMu], f_0*sigmaxy_0[iMu], f_0*sigmazz_0[iMu]);
             fprintf(fileOut,"%e\t%e\t%e\t%e\t", T[nn],f_0*density[iMu][nn], 1.0-f_0*density[iMu][nn], f*Cv[iMu][nn]);
             fprintf(fileOut,"%e\t%e\t%e\t", f*sigma_xx[iMu][nn], f*sigma_xy[iMu][nn], f*sigma_zz[iMu][nn] );
